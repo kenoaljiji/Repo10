e472f20e6f48ddaf01c0d09b385404d60c468455
kenoaljiji <studioaljiji@gmail.com>
2021-09-22 19:17:41 +0200

    Update object properties

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,15 +1,17 @@
 var school = {
   name: "Arena",
-  location: 4,
-  established: 1,
+  location: "Sarajevo",
+  established: "2020",
 };
 
 function myFunction(name) {
-  school["name"] = name;
-
+  //Only change code below this line
+  school.name = name;
   return school;
+
+  //Only change code above this line
 }
 
 console.log(myFunction("Paragon"));
 
-module.exports = myFunction(school, myFunction);
+module.exports = { school, myFunction };
