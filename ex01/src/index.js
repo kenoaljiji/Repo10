cfa9ebb819e6b4d8c9d51c896a3bc6359240fcf4
kenoaljiji <studioaljiji@gmail.com>
2021-09-22 14:44:23 +0200

    add object property and assigning to a myFunction argument

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+var myPet = {
+  species: "American Shorthair Cat",
+  name: "Minnie",
+  legs: 4,
+  friends: ["Dodik", "Bakir"],
+};
+
+function myFunction(myObj) {
+  //Only change code below this line
+  return myObj;
+  //Only change code above this line
+}
+
+console.log(myFunction(myPet));
+
+module.exports = { myPet, myFunction };
